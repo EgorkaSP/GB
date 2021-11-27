856625654ddbeb60847f00c9e36af679d2a0267c
kristina <c.listseva@yandex.ru>
2021-11-27 20:44:56 +0300

    решила 12

---
+++ seminar2.5/Program.cs
@@ -0,0 +1,9 @@
+﻿// 12. удалить вторую цифру из 3 значного числа
+int three_digit_number=new Random().Next(100,1000);
+Console.WriteLine(three_digit_number);
+int first_digit=three_digit_number/100;
+int last_digit= three_digit_number%10;
+Console.WriteLine(first_digit+ "" +last_digit);
+//String First_digit = Convert.ToString(first_digit);
+//String Last_digit = Convert.ToString(last_digit);
+//Console.Write(First_digit+" "+Last_digit);
