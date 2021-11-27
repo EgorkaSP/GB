5307e9b79a94bc89eb91651d35453a25a6347f6a
kristina <c.listseva@yandex.ru>
2021-11-27 23:39:18 +0300

    решила 14

---
+++ seminar2.7/Program.cs
@@ -0,0 +1,13 @@
+﻿// 14. найти третью цифру числа или сообщить что ее нет
+int number=new Random().Next(1,1000);
+Console.WriteLine(number);
+int result=number%10;
+if(number>100)
+{
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("3-ей цифры нет");
+}
+
