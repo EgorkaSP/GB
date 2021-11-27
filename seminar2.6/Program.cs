c7a8ca975f2d13f56431d93a1d3c18a6f858eeb8
kristina <c.listseva@yandex.ru>
2021-11-27 20:53:58 +0300

    решила 13

---
+++ seminar2.6/Program.cs
@@ -0,0 +1,13 @@
+﻿// 13. выяснить кратно ли число заданному, если нет, то вывести остаток
+int Const=3;
+int some_number=new Random().Next(1,100);
+Console.WriteLine(some_number);
+int result= some_number%Const;
+if(result==0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine(result);
+}
