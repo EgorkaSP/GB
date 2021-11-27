a71e3e3d291ac665dc325928730de306cfe2299f
kristina <c.listseva@yandex.ru>
2021-11-27 15:19:52 +0300

    решила 7 и 8 задачу

---
+++ SEMINAR_2/Program.cs
@@ -0,0 +1,23 @@
+﻿void PrintArray(int[] natural)
+{
+    int size=natural.Length;
+    int index=0;
+        while(index<size)
+        {
+            Console.WriteLine(natural[index]);
+            index++;
+        }
+}
+int[] array={-5,-4,-3,-2,-1,0,1,2,3,4,5};
+PrintArray(array);
+Console.WriteLine();
+
+// или так
+Console.WriteLine("Введите число N");
+int N= int.Parse(Console.ReadLine());
+int count=-N;
+while(count<N+1)  // или count<=N
+{
+    Console.WriteLine(count);
+    count++;
+}
---
+++ seminar2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿// 8. показать четные числа от 1 до N
+Console.WriteLine("введите число N");
+int N=int.Parse(Console.ReadLine());
+int count=1;
+while(count <= N)
+{
+    if(count % 2 == 0)
+    {
+        Console.WriteLine(count);
+            }
+    
+count++;
+}
