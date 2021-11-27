aa116a31dbe883e98ae25372a0d75535e829a7a7
kristina <c.listseva@yandex.ru>
2021-11-27 15:47:05 +0300

    решила 9 задачу

---
+++ seminar2.2/Program.cs
@@ -0,0 +1,5 @@
+﻿// 9. показать последнюю цифру 3значного числа
+Console.WriteLine("введите 3 значное число");
+int three_digit_number=int.Parse(Console.ReadLine());
+int last_digit= three_digit_number%10;
+Console.WriteLine(last_digit);
