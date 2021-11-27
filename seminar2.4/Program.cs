ddccc7226b9b13c015b5699621e3538f01864cf7
kristina <c.listseva@yandex.ru>
2021-11-27 20:11:15 +0300

    решила 11

---
+++ seminar2.4/Program.cs
@@ -0,0 +1,17 @@
+﻿// 11. Дано число из отрезка 10, 99, показать наибольшую цифру числа
+int find = new Random().Next(10,100);
+Console.WriteLine(find);
+int first_digit=find/10;
+int second_digit=find%10;
+if(first_digit>second_digit)
+{
+    Console.WriteLine(first_digit);
+}
+if(second_digit>first_digit)
+{
+    Console.WriteLine(second_digit);
+}
+else if(second_digit==first_digit)
+{
+    Console.WriteLine("они равны");
+}
