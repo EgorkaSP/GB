efd2a943fda0f43bc4f67530dc8f5694ed71d60a
kristina <c.listseva@yandex.ru>
2021-11-27 15:59:24 +0300

    решила 10 задачу

---
+++ seminar2.3/Program.cs
@@ -0,0 +1,7 @@
+﻿// 10. показать вторую цифру 3 значного числа
+Console.WriteLine("введите 3 значное число");
+int three_digit_number=int.Parse(Console.ReadLine());
+int last_digit=three_digit_number % 100;
+int second_digit=last_digit / 10;
+Console.WriteLine(second_digit);
+
