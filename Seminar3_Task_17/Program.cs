675d33ce1017f9e999ec55be2c280c136d7c896e
kristina <c.listseva@yandex.ru>
2021-11-29 19:30:28 +0300

    решила 17

---
+++ Seminar3_Task_17/Program.cs
@@ -0,0 +1,12 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("введите 2 числа");
+int Num1=int.Parse(Console.ReadLine());
+int Num2=int.Parse(Console.ReadLine());
+if(Num1*Num1==Num2 || Num2*Num2==Num1 )
+{
+    Console.WriteLine(" одно квадрат другого");
+     }
+  else
+  {
+      Console.WriteLine("  не квадрат ");
+  }   
