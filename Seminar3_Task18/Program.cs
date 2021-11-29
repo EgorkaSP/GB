01895fe4cc39a253198d4c88a6e79586922aec2e
kristina <c.listseva@yandex.ru>
2021-11-29 21:34:26 +0300

    решила 18

---
+++ Seminar3_Task18/Program.cs
@@ -0,0 +1,11 @@
+﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool x= false;
+bool y= true;
+if(!(x||y) == !x && !y)
+{
+    Console.WriteLine("утверждение истинно");
+}
+else
+{
+    Console.WriteLine("утверждение ложно");
+}
