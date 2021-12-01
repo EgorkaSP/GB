736c284dafd997a6592a046c9e782bae2c6d8555
kristina <c.listseva@yandex.ru>
2021-12-01 22:02:05 +0300

    20, но есть вопросы

---
+++ Seminar3_Task20/Program.cs
@@ -0,0 +1,24 @@
+﻿// 20. Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("введите номер четверти");
+int quarterNumber=int.Parse(Console.ReadLine());
+if(quarterNumber==1)
+{
+    Console.WriteLine("x>0,y>0");
+}
+if(quarterNumber==2)
+{
+    Console.WriteLine("x<0,y>0");
+}
+if(quarterNumber==3)
+{
+    Console.WriteLine("x<0,y<0");
+}
+if(quarterNumber==4)
+{
+    Console.WriteLine("x>0,y<0");
+}
+if(quarterNumber>4)
+{
+    Console.WriteLine("есть только 4 четверти");
+}
