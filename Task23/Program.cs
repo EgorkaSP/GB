324a6277cba1bc9e5739dea05afab17c40357274
kristina <c.listseva@yandex.ru>
2021-12-04 00:39:44 +0300

    через цикл фор

---
+++ Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+Console.WriteLine("введите число N");
+int N=int.Parse(Console.ReadLine());
+for (int i = 2; i <= N; i++)
+{
+    Console.WriteLine($"{i}*{i}={i*i}");
+    Console.WriteLine();
+}
+
+
