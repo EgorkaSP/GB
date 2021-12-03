ba3889d62207675ab909b00608a11521fb87f9c9
kristina <c.listseva@yandex.ru>
2021-12-04 00:59:07 +0300

    через фор и коннактетацию

---
+++ Task24/Program.cs
@@ -0,0 +1,11 @@
+﻿// 24. Найти кубы чисел от 1 до N
+Console.WriteLine("введите число N");
+double N=int.Parse(Console.ReadLine());
+for (double i = 2; i <= N; i++)
+{
+    Console.Write(i+ " в кубе ");
+    double cub=Math.Pow(i, 3);
+    Console.WriteLine(cub);
+}
+
+
