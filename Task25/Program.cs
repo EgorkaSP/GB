e6b8cd536c04a20ca06617d101a815671d431d35
kristina <c.listseva@yandex.ru>
2021-12-04 14:58:21 +0300

    коротенько 25

---
+++ Task25/Program.cs
@@ -0,0 +1,9 @@
+﻿// 25.Найти сумму чисел от 1 до А или N
+Console.WriteLine("введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = N * (N + 1) / 2;
+Console.WriteLine("сумма чисел от 1 до "  + N + "=" + sum);
+
+
+
+
