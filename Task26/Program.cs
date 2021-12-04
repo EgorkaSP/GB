f4dd346951257fc933c9e8ec32a009ca22ad9026
kristina <c.listseva@yandex.ru>
2021-12-04 15:45:29 +0300

    26 через метод

---
+++ Task26/Program.cs
@@ -0,0 +1,19 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+int Pow(int A,int B)
+{ int result=A;
+    for (int i = 1; i < B; i++)
+    {
+        result*=A;
+        
+    }
+return result;
+}
+
+Console.WriteLine("Ведите а");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите b");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.Write("результат=");
+
+int pow=Pow(a,b);
+Console.Write(pow);
