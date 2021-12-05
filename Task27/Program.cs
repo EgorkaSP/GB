beb945582a3a27b93a399cf0793d36adf96a4673
kristina <c.listseva@yandex.ru>
2021-12-05 14:31:08 +0300

    лекция 4, доделай,

---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// 27. Определить количество цифр в числе
+
+ int CountDigits(int n)
+{   int count = 0;
+    if(n==0) count=1;
+     else                     
+    while (n != 0)
+    {
+        n /= 10;
+        count++;
+    }
+
+    return count;
+}
+
+Console.WriteLine("введите число");
+int number=Convert.ToInt32(Console.ReadLine());
+int num=CountDigits(number);
+Console.WriteLine(num);
