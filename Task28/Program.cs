877bd717eb957421fad525a4d31444097bb1f22a
kristina <c.listseva@yandex.ru>
2021-12-05 14:31:46 +0300

    28

---
+++ Task28/Program.cs
@@ -0,0 +1,11 @@
+﻿// 28. Подсчитать сумму цифр в числе
+Console.WriteLine("введите число");
+int N=Convert.ToInt32(Console.ReadLine());
+int sum=0;
+while(N!=0)
+{
+    sum+=N%10;
+    N/=10;
+    }
+Console.WriteLine( "сумма чисел " + sum);
+
