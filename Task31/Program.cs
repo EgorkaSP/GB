49a2e5ca4f63ecee93abfc90dabf820e572c26c1
kristina <c.listseva@yandex.ru>
2021-12-09 21:32:10 +0300

    31 32

---
+++ Task31/Program.cs
@@ -0,0 +1,22 @@
+﻿//31. Задать массив из 8 элементов и вывести их на экран
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i]=new Random().Next(1,100000);
+        
+        
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
+
+int[] Array=new int[8];
+FillArray(Array);
+PrintArray(Array);
---
+++ Task32/Program.cs
@@ -0,0 +1,19 @@
+﻿// 32.Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int[] arr=new int[8];
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]=new Random().Next(0,2);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+FillArray(arr);
+PrintArray(arr);
