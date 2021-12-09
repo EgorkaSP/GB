549ef4ffeaaeb852d1005e9cc4c883668d7b7c6f
kristina <c.listseva@yandex.ru>
2021-12-09 13:45:46 +0300

    29 цикл фаакториал

---
+++ Task29/Program.cs
@@ -0,0 +1,10 @@
+﻿// 29 найти произведение чисел от 1 до N
+Console.WriteLine("введите N");
+int N=Convert.ToInt32(Console.ReadLine());
+int product=1;
+for (int i = 2; i <= N; i++)
+{
+     product*=i;
+            
+}
+Console.WriteLine("произведение равно " + product);
