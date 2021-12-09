2df46bfa72ca57ba8d04ebb6411dbf5200190862
kristina <c.listseva@yandex.ru>
2021-12-09 14:21:46 +0300

    30 но есть вопросики

---
+++ Task30/Program.cs
@@ -0,0 +1,12 @@
+﻿// 30.Показать кубы чисел, заканчивающихся на четную цифру
+
+double number=Convert.ToDouble(new Random().Next(1,10000));
+Console.WriteLine(number);
+double lastDigit=number%10;
+if(lastDigit%2==0)
+{
+    double result= Math.Pow(number,3);
+    Console.WriteLine(result);
+}
+else Console.WriteLine("последняя цифра нечетная");
+
