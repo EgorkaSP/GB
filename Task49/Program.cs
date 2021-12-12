991e7d30c67f29b8e3e9d9532873cc0d1582d007
kristina <c.listseva@yandex.ru>
2021-12-12 13:38:13 +0300

    49, доработай

---
+++ Task49/Program.cs
@@ -0,0 +1,24 @@
+﻿//49. Показать двумерный массив размером m×n заполненный вещественными числами
+double[,] matrix=new double[5,7];
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i <matr.GetLength(0); i++)
+    {
+        for (int j = 0; j <matr.GetLength(1); j++)
+        {
+            matr[i,j]=new Random().NextDouble;
+        }
+    }
+}
+
+void PrintArray(double[,] Matrica)
+{
+    for (int i = 0; i <Matrica.GetLength(0); i++)
+    {
+        for (int j = 0; j <Matrica.GetLength(1); j++)
+        {
+            Console.WriteLine($"{Matrica[i,j]}" );
+        }
+        Console.WriteLine();
+    }
+}
