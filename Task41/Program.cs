d98ccdd36657a3aa64628b8ec138fcd390f1c6bc
kristina <c.listseva@yandex.ru>
2021-12-14 13:04:28 +0300

    41 недоделала, илья рекурсия

---
+++ Task41/Program.cs
@@ -0,0 +1,11 @@
+﻿// 41.Выяснить являются ли три числа сторонами треугольника
+
+int a=Convert.ToInt32(Console.ReadLine());
+int b=Convert.ToInt32(Console.ReadLine());
+int c= Convert.ToInt32(Console.ReadLine());
+
+if( ((a+b)>c) && ((c+a)>b) && ((b+c)>a) ) Console.WriteLine("это треугольник");
+else
+{
+    Console.WriteLine("это не треугольник");
+}
