af44d28830e3ce61a1ba554baf09b23a64bbabe6
kristina <c.listseva@yandex.ru>
2021-12-15 13:49:09 +0300

    50

---
+++ Task50/Program.cs
@@ -0,0 +1,47 @@
+﻿// 50. В двумерном массиве n×k заменить четные элементы на противоположные
+void FillArray(int[,] matr, int min,int max)
+{
+    for (int i = 0; i <matr.GetLength(0); i++)
+    {
+        for (int j = 0; j <matr.GetLength(1); j++)
+        {
+            matr[i,j]=new Random().Next(min,max);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i <matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j <matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("введите n ");
+int lengthN=int.Parse(Console.ReadLine());
+Console.Write("введите k ");
+int lengthK=int.Parse(Console.ReadLine());
+
+int[,] Matrica=new int[lengthN,lengthK];
+FillArray(Matrica,1,10);
+PrintArray(Matrica);
+
+void Mirror(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] *= -1;
+        }
+    }
+}
+
+Mirror(Matrica);
+Console.WriteLine();
+PrintArray(Matrica);
