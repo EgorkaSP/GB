155751a15828df148e1ce15e627c375a25381496
kristina <c.listseva@yandex.ru>
2021-12-15 13:51:22 +0300

    50 переделала немного

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -37,7 +37,7 @@ void Mirror(int[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] *= -1;
+            if(array[i,j]%2==0) array[i, j] *= -1;
         }
     }
 }
