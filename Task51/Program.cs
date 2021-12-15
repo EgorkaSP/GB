dbda2de1bec441d4dafbb62a607f0663833e25db
kristina <c.listseva@yandex.ru>
2021-12-15 15:17:08 +0300

    51

---
+++ Task51/Program.cs
@@ -0,0 +1,15 @@
+﻿// 51. Задать двумерный массив следующим правилом: Aₘₙ = m+n
+Console.WriteLine("введите m: ");
+int lengthM=int.Parse(Console.ReadLine());
+Console.WriteLine("введите n ");
+int lengthN=int.Parse(Console.ReadLine());
+
+int[,] A=new int[lengthM,lengthN];
+for (int i = 0; i <A.GetLength(0); i++)
+{
+    for (int j = 0; j <A.GetLength(1); j++)
+    {
+        Console.Write($"{i} + {j} ={A[i,j]=i+j}  ");
+    }
+    Console.WriteLine();
+}
