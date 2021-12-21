1d3a7c54099bb981d5c3cf81b9c0395f500c6ede
kristina <c.listseva@yandex.ru>
2021-12-21 23:42:22 +0300

    58, 59 с сергеем

---
+++ Task58/Program.cs
@@ -0,0 +1,71 @@
+﻿// 58. Написать программу, которая в двумерном массиве заменяет строки на столбцы
+// или сообщить, что это невозможно (в случае, если матрица не квадратная).
+
+void FillArray(int[,] matrix, int m, int n)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(m, n);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("введите m: ");
+int lengthM = int.Parse(Console.ReadLine());
+Console.Write("введите n:");
+int lengthN = int.Parse(Console.ReadLine());
+int[,] matrica1 = new int[lengthM, lengthN];
+FillArray(matrica1, 1, 10);
+// PrintArray(matrica1);
+// Console.WriteLine();
+// if(lengthM==lengthN )
+// {
+//     Transfer();
+
+// }
+// else Console.WriteLine("матрица не квадратная");
+
+int[,] Transfer(int[,] arr)
+{ int column=arr.GetLength(0);
+int row=arr.GetLength(1);
+    int[,] result = new int[row,column];
+    for (int i = 0; i <row ; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+
+            result[j, i] = arr[i, j];
+
+
+
+        }
+    }
+    return result;
+} 
+DateTime saveTime = DateTime.Now;
+
+int[,] res = Transfer(matrica1);
+Console.WriteLine($"{DateTime.Now - saveTime}");
+
+// for (int i = 0; i < res.GetLength(0); i++)
+// {
+//     for (int j = 0; j < res.GetLength(1); j++)
+//     {
+//         Console.Write($"{res[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
---
+++ Task59/Program.cs
@@ -0,0 +1,70 @@
+﻿// 59. В прямоугольной матрице найти строку с наименьшей суммой элементов.
+void FillArray(int[,] matrix, int m, int n)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(m, n);
+        }
+
+    }
+}
+
+void PrintArray(int[,] matr)
+{ int sum=0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {  sum=0;
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            sum+=matr[i,j];
+                        Console.Write($"{matr[i, j]} ");
+        }
+              Console.WriteLine("сумма строки " +sum);
+    }
+}
+Console.Write("введите m: ");
+int lengthM = int.Parse(Console.ReadLine());
+Console.Write("введите n:");
+int lengthN = int.Parse(Console.ReadLine());
+int[,] matrica = new int[lengthM, lengthN];
+
+FillArray(matrica, 1, 10);
+PrintArray(matrica);
+Console.WriteLine();
+
+
+
+if (lengthM != lengthN)
+{
+   Console.WriteLine(MinSumm(matrica));
+}
+else Console.WriteLine("матрица не прямоугольная");
+
+
+int MinSumm(int[,] array)
+{
+    int temp = -1;
+    int s = 0;
+    int sMin = int.MaxValue;  // f12
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        s = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            s += array[i, j];
+
+        }
+        if (s <= sMin)
+        {
+            sMin = s;
+            temp = i;
+
+        }
+
+    }
+
+    return temp;
+}
+
+//работает, убери тест
